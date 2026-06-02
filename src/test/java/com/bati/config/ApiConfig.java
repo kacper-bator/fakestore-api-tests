4423eb9a3b6f2bad3c022373e40e5ea15837a0de
kacper-bator <kacper.bator@op.pl>
2026-06-02 23:09:16 +0200

    test: add products endpoint tests

--- src/test/java/com/bati/config/ApiConfig.java
+++ src/test/java/com/bati/config/ApiConfig.java
@@ -1,4 +1,5 @@
 package com.bati.config;
 
 public class ApiConfig {
+    public static final String BASE_URL = "https://fakestoreapi.com";
 }
--- src/test/java/com/bati/tests/ProductsIT.java
+++ src/test/java/com/bati/tests/ProductsIT.java
@@ -1,4 +1,38 @@
 package com.bati.tests;
 
+import com.bati.config.ApiConfig;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThan;
+
 public class ProductsIT {
+
+    @BeforeAll
+    static void setup() {
+        RestAssured.baseURI = ApiConfig.BASE_URL;
+    }
+
+    @Test
+    void shouldReturnAllProducts() {
+        given()
+                .when()
+                .get("/products")
+                .then()
+                .statusCode(200)
+                .body("size()", greaterThan(0));
+    }
+
+    @Test
+    void shouldReturnSingleProduct() {
+        given()
+                .when()
+                .get("/products/1")
+                .then()
+                .statusCode(200)
+                .body("id", equalTo(1));
+    }
 }
